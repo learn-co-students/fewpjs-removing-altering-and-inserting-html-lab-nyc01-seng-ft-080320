c17a882ea92ace90f1f85acb5fdc61f2e6429902
Mattie43 <ericksenm93@ymail.com>
2020-09-11 18:33:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here!
+
+// let element = document.createElement('p')
+
+// document.body.appendChild(element)
+
+// let ul = document.createElement('ul')
+ 
+// for (let i = 0; i < 3; i++) {
+//   let li = document.createElement('li')
+//   li.innerHTML = (i + 1).toString()
+//   ul.appendChild(li)
+// }
+ 
+// element.appendChild(ul)
+
+// element.style.backgroundColor = '#27647B';
+// element.style.textAlign = 'center';
+// element.style.fontSize = '50px';
+// ul.style.textAlign = 'left';
+
+// ul.removeChild(ul.querySelector('li:nth-child(2)'))
+
+// tests
+main.remove()
+
+// let newHeader = document.createElement('h1')
+let newHeader = document.createElement('h1')
+// document.body.appendChild(newHeader)
+let champion = "YOUR-NAME is the champion"
+newHeader.appendChild(champion)
