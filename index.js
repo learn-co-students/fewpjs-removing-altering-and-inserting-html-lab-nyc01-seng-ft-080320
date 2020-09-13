ca79f4c7756e2e48213f72ae5e1fe16626665819
anickel101 <anson.nickel@gmail.com>
2020-09-13 08:28:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Learn #1
+document.querySelector('main#main').remove()
+
+//Learn #2
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Anson is the champion'
+
+
