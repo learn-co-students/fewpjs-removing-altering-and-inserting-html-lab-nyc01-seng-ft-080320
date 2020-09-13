42af9291829da04db45a76b108c4ec1237899e16
royFloresNyc <royfloresnyc@gmail.com>
2020-09-12 20:53:50 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.innerHTML = "Roy is the champion!";
+newHeader.id = 'victory';
+
+const body = document.getElementsByTagName('body');
+
+body[0].appendChild(newHeader);
